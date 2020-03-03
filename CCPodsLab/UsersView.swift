780804ebdb70a395cf5b4ab8e51f8989ec21b4dc
Juan C <juanceballos@pursuit.org>
2020-03-03 10:58:13 -0500

    setting up ccpods lab

---
+++ CCPodsLab/UsersView.swift
@@ -0,0 +1,37 @@
+//
+//  UsersView.swift
+//  CCPodsLab
+//
+//  Created by Juan Ceballos on 3/3/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+
+class UsersView: UIView {
+
+    private lazy var usersTableView: UITableView =  {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit()   {
+        
+    }
+    
+    private func setupUsersTableViewConstraints()   {
+        
+    }
+    
+}
---
+++ CCPodsLab/UsersViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CCPodsLab
+//
+//  Created by Juan Ceballos on 3/3/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
